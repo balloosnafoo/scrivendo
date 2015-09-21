4b0db9d4ff2fe28ca6ed52cbe4b2b5f5b55b3a89
Matthew Skinner <skinnerm@email.arizona.edu>
2015-09-20 22:27:15 -0700

    write navbar view

---
+++ app/assets/javascripts/views/navbar.js
@@ -0,0 +1,31 @@
+Scrivendo.Views.Navbar = Backbone.View.extend({
+  template: JST['navbar/navbar'],
+
+  initialize: function (options) {
+    this.router = router;
+
+    this.listenTo(this.router, "route", this.handleRoute)
+  },
+
+  render: function () {
+    var content = this.template();
+    this.$el.html(content);
+    return this;
+  },
+
+  handleRoute: function (routeName, routeParams) {
+    this.$el.find(".active").removeClass("active");
+    this.$el.find("." + routeName).addClass("active");
+  },
+
+  logout: function (event) {
+    event.preventDefault();
+    $.ajax({
+      url: "/session/",
+      type: "DELETE",
+      success: function () {
+        window.location = "/";
+      }
+    });
+  }
+});
