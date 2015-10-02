99140460b78ee3e7e8f48e192d45beb4a5c62e5d
Matthew Skinner <skinnerm@email.arizona.edu>
2015-10-01 22:31:23 -0700

    write basic seed file for development

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
 gem 'backbone-on-rails'
 gem 'bootstrap-sass'
+gem 'faker'
 
 group :development, :test do
   gem 'pry-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create user for testing
+User.create(
+  username: "balloo",
+  password: "password"
+)
+
+# Create random users
+10.times do |i|
+  User.create(
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    password: "password"
+  )
+end
+
+# Create stories
+50.times do |i|
+  Story.create(
+    title: Faker::Lorem.sentence(3),
+    description: Faker::Lorem.paragraphs(2).join("\n"),
+    user_id: rand(User.count) + 1
+  )
+end
+
+# Create tellings
+200.times do |i|
+  body = (0..6).map { |_| Faker::Lorem.paragraph(10) }.join("\n")
+  Telling.create(
+    title: Faker::Lorem.sentence(3),
+    description: Faker::Lorem.paragraphs(2).join("\n"),
+    user_id: rand(User.count) + 1,
+    story_id: rand(Story.count) + 1,
+    body: body,
+  )
+end
+
+200.times do |i|
+  body = (0..2).map { |_| Faker::Lorem.paragraph(5) }.join("\n")
+  Critique.create(
+    body: body,
+    critic_id: rand(User.count) + 1,
+    telling_id: rand(Telling.count) + 1
+  )
+end
